ed62cee916d17cb9905f6d69b459278cd10d537d
Aarno Aukia <aarno.aukia@vshn.ch>
2019-02-21 22:33:10 +0100

    initial commit

---
+++ app.py
@@ -0,0 +1,31 @@
+from kubernetes import client, config
+from pprint import pprint
+import random
+import os
+import RPi.GPIO as GPIO
+
+namespace='appuio-salesdemo1'
+selector='app=static-go'
+channel="2"
+
+# Configs can be set in Configuration class directly or using helper utility
+config.load_kube_config()
+v1 = client.CoreV1Api()
+
+def chaos():
+    pods = v1.list_namespaced_pod(namespace=namespace, label_selector=selector)
+    #for pod in pods.items:
+    #    print("%s\t%s\t%s" % (pod.metadata.name,
+    #                          pod.status.phase,
+    #                          pod.spec.node_name))
+    podname = random.choice(pods.items).metadata.name
+    print("killing {0}".format(podname))
+    v1.delete_namespaced_pod(podname, namespace, client.V1DeleteOptions(grace_period_seconds=0))
+
+def my_callback():
+    print("called")
+
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(channel, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.add_event_detect(channel, GPIO.RISING, callback=my_callback, bouncetime=200)
+
